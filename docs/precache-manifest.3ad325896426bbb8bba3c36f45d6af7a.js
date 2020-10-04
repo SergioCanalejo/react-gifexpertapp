4ba6d19a8330c65d2ee9928a04646670b07aefee
Sergio Canalejo <canalejo.sergio@gmail.com>
2020-10-04 13:29:10 +0200

    Docs added

---
+++ docs/precache-manifest.3ad325896426bbb8bba3c36f45d6af7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40d571424b2ce053a808c0519d083f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03384826a06c387174ef",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "e8165c4fe895ef52369e",
+    "url": "/static/css/main.79582346.chunk.css"
+  },
+  {
+    "revision": "03384826a06c387174ef",
+    "url": "/static/js/2.54150602.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.54150602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8165c4fe895ef52369e",
+    "url": "/static/js/main.67cd0c73.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
